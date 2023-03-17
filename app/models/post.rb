a7809419163bfcc0f4636e82d7575ac37dbf512f
Michelle_Mungai <chellymungai@gmail.com>
2023-03-17 14:56:26 +0300

    Completed assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+  validate :must_be_title
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+
+  def must_be_title
+    if title == "True Facts"
+      errors.add(:title, "Invalid title")
+    end
+  end
 end
